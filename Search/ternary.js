369197623abbaba7c76d1b69bf4c159bfc7a26af
Dilpreet Singh <singhsince94@gmail.com>
2018-01-08 23:05:24 +0530

    Adds ternary search

---
+++ Search/ternary.js
@@ -0,0 +1,16 @@
+function ternarySearch(arr, l, r, x) {
+  if (r >= l) {
+      var mid1 = l + (r - l)/3;
+      var mid2 = r - 1 - (r - l)/3;
+
+      if (arr[mid1] == x) return mid1;
+      if (arr[mid2] == x) return mid2;
+
+      if (arr[mid1] > x) return ternarySearch(arr, l, mid1-1, x);
+      if (arr[mid2] < x) return ternarySearch(arr, mid2+1, r, x);
+
+      return ternarySearch(arr, mid1+1, mid2-1, x);
+   }
+  return -1;
+}
+ternarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, 9, 7);
